dc45801f7c52934379168c4805c383de54856e64
Laura Jazmin Anderson <jazminandersonxo@gmail.com>
2022-05-15 18:19:34 -0700

    node questions

---
+++ index.js
@@ -0,0 +1,53 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs'); 
+
+// TODO: Create an array of questions for user input
+const questions = [ 
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description about your project.'
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: "Choose a license for your application.",
+        choices: [ 'MIT', 'GPL', 'BSD']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is yout Github username?'
+    },
+    {
+        type: 'input',
+        name: 'contact',
+        message: 'Enter your email address.',
+    }
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(filename, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+      }
+
+// TODO: Create a function to initialize app
+function init() {
+        inquirer.prompt(questions)
+          .then((data) => {
+            console.log(data)
+})
+}
+
+// Function call to initialize app
+init();
